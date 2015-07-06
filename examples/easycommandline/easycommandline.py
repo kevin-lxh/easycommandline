fac000d0bf0f34c27ed52a120a5d4d06fa0e3917
Tracy <me@tracycool.com>
2015-07-06 20:17:27 +0800

    refactoring

--- examples/easycommandline/easycommandline.py
+++ examples/easycommandline/easycommandline.py
@@ -1,17 +1,17 @@
 import sys
 import re
 
+
 class Option(object):
-    def __init__(self, option): # params (symbol, fullname, description, action, initial_arg)
-        if isinstance(option, (list, tuple)) == False or len(option) < 2:
+    def __init__(self, info): # params (symbol, fullname, description, action, initial_arg)
+        if isinstance(info, (list, tuple)) == False or len(info) < 2:
             raise Exception('option name missing')
 
-        self.symbol = option[0]
+        self.symbol = info[0]
 
-        self.fullname = option[1].strip()
+        self.fullname = info[1].strip()
         name_components = self.fullname.split(' ')
         self.name = name_components[0]
-
 
         if not Option.is_valid_symbol(self.symbol):
             raise Exception('`({0}, {1}, ...)` is invalid, the symbol must start with `-`, such as `-f`'.format(self.symbol, self.fullname))
@@ -19,11 +19,11 @@ class Option(object):
         if not Option.is_valid_name(self.name):
             raise Exception('`({0}, {1}, ...)` is invalid, the option name must start with `--`, such as `--foo`'.format(self.symbol, self.fullname))
 
-        self.description = option[2] if len(option) >= 3 else ''
-        self.action = option[3] if len(option) >= 4 else None
+        self.description = info[2] if len(info) >= 3 else ''
+        self.action = info[3] if len(info) >= 4 else None
 
-        self.initial_arg_set = len(option) >= 5
-        self.initial_arg = option[4] if self.initial_arg_set else None
+        self.initial_arg_set = len(info) >= 5
+        self.initial_arg = info[4] if self.initial_arg_set else None
 
         self.arg_required = self.action is not None
 
@@ -36,6 +36,101 @@ class Option(object):
     @classmethod
     def is_valid_name(cls, text):
         return (re.match(r'^--[a-zA-Z]', text) is not None)
+
+
+
+
+class OptionParser(object):
+    def __init__(self, options, argv):
+        self.__options = options[:];
+        self.__argv = argv[:]
+        self.__argv.pop(0)
+
+
+    def parse(self):
+        start_index = 0
+
+        name_to_argument_mapping = {}
+        for option in self.__options:
+            name_to_argument_mapping[option.name] = option.initial_arg
+
+        while True:
+            location, length = self.__range_for_option_and_argv(start_index)
+
+            if length == 0: # done
+                break
+
+            symbol_or_name = self.__argv[location]
+            option = self.__find_option_by_symbol_or_name(symbol_or_name)
+            argv_for_option = self.__argv[(location+1) : (location+length)]
+
+            argument = self.__parse_for_an_option(option, argv_for_option)
+            name_to_argument_mapping[option.name] = argument
+
+            start_index = location + length
+
+        return name_to_argument_mapping
+
+
+    def __parse_for_an_option(self, option, argv):
+        a_single_argument = option.initial_arg
+
+        if len(argv) == 0:
+            if option.arg_required:
+                raise Exception('option `{0}, {1}` argument missing'.format(option.symbol, option.fullname))
+            else:
+                a_single_argument = True
+
+        else:
+            if option.action:
+                if option.initial_arg_set: # reduce
+                    for arg in argv:
+                        previous_value = a_single_argument
+                        a_single_argument = option.action(arg, previous_value)
+
+                else: # call action with first argument
+                    a_single_argument = option.action(argv[0])
+
+            else:
+                a_single_argument = argv[0]
+
+        return a_single_argument
+
+
+    def __range_for_option_and_argv(self, start_index):
+        location = None
+        length = 0
+
+        for index in range(start_index, len(self.__argv)):
+            text = self.__argv[index]
+
+            is_option_symbol_or_name = ( Option.is_valid_symbol(text) or Option.is_valid_name(text) )
+            if is_option_symbol_or_name:
+                if self.__find_option_by_symbol_or_name(text) is None:
+                    raise Exception("unknown option `{0}`".format(text))
+                
+            if is_option_symbol_or_name:
+                if location is None:
+                    location = index
+                    length = 1
+                else:
+                    break # done
+
+            else: # an argument
+                if location is not None:
+                    length += 1
+
+        return (location, length)
+
+
+    def __find_option_by_symbol_or_name(self, symbol_or_name):
+        for option in self.__options:
+            if option.symbol == symbol_or_name or option.name == symbol_or_name:
+                return option
+
+        return None
+    
+
 
 
 class Commander(object):
@@ -62,9 +157,9 @@ class Commander(object):
 
 
     def options(self, *options):
-        for a_list in options:
+        for option_info in options:
             try:
-                option = Option(a_list)
+                option = Option(option_info)
             except Exception, e:
                 self.__print_exception(e)
                 sys.exit()
@@ -81,94 +176,18 @@ class Commander(object):
         self.__max_length_of_description = max(len(option.description), self.__max_length_of_description)
 
 
-    def __find_option_by_symbol_or_name(self, symbol_or_name):
-        for option in self.__options:
-            if option.symbol == symbol_or_name or option.name == symbol_or_name:
-                return option
-
-        return None
-
-
     def parse(self, argv):
-        argv = argv[:]
-        self.__script_name = argv.pop(0)
-
-        start_index = 0
-        while True:
-            try:
-                location, length = self.__range_for_option(start_index, argv)
-            except Exception, e:
-                self.__print_exception(e)
-                sys.exit()
-
-            if length == 0: # done
-                break
-
-            # find option
-            symbol_or_name = argv[location]
-            option = self.__find_option_by_symbol_or_name(symbol_or_name)
-
-            # parse argv
-            arguments_for_option = argv[(location+1) : (location+length)]
-            if len(arguments_for_option) == 0:
-                if option.arg_required:
-                    self.__print_exception('option `{0}, {1}` argument missing'.format(option.symbol, option.fullname))
-                    sys.exit()
-                else:
-                    self.__name_to_argument_mapping[option.name] = True
-
-            else:
-                if option.action:
-                    if option.initial_arg_set: # reduce
-                        for value in arguments_for_option:
-                            previous_value = self.__name_to_argument_mapping[option.name]
-                            new_value = option.action(value, previous_value)
-                            self.__name_to_argument_mapping[option.name] = new_value
-
-                    else: # call action with first argument
-                        value = option.action(arguments_for_option[0])
-                        self.__name_to_argument_mapping[option.name] = value
-
-                else:
-                    value = arguments_for_option[0]
-                    self.__name_to_argument_mapping[option.name] = arguments_for_option[0]
-
-            # start_index
-            start_index = location + length
-
-        self.__did_parse_argv()           
+        self.__script_name = argv[0]
+        try:
+            self.__name_to_argument_mapping = OptionParser(self.__options, argv).parse()    
+        except Exception, e:
+            self.__print_exception(e)
+            sys.exit()
+        
+        self.__did_parse_options()
 
 
-    def __range_for_option(self, start_index, argv):
-        location = None
-        length = 0
-
-        for index in range(start_index, len(argv)):
-            text = argv[index]
-
-            is_option = False
-            if Option.is_valid_symbol(text) or Option.is_valid_name(text):
-                if self.__find_option_by_symbol_or_name(text):
-                    is_option = True
-                else:
-                    raise Exception("unknown option `{0}`".format(text))
-                
-            # location, length
-            if is_option:
-                if location is None:
-                    location = index
-                    length = 1
-                else:
-                    break
-
-            else: 
-                if location is not None:
-                    length += 1
-
-        return (location, length)
-    
-
-    def __did_parse_argv(self):
+    def __did_parse_options(self):
         if self.arg('help'):
             self.__print_help()
             sys.exit()
@@ -184,7 +203,7 @@ class Commander(object):
 
     def arg(self, attr_name):
         key = '--' + attr_name
-        return self.__name_to_argument_mapping[key]
+        return self.__name_to_argument_mapping.get(key)
 
 
     def __print_help(self):
@@ -208,4 +227,6 @@ class Commander(object):
         print('\n  error: {0}\n'.format(e))
 
 
-program = eval('Commander()') # eval, avoid syntax checking
+
+
+program = eval('Commander()') # eval, avoid methods checking to `program`
